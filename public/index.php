f32d6d6757f89efdf4a34f2ab28872909d12acdb
Viral Joshi <viralj@users.noreply.github.com>
2014-11-04 12:55:06 -0600

    Some explanation added.

--- public/index.php
+++ public/index.php
@@ -1,26 +1,26 @@
 <?php
+ob_start();
 
+//default timezone is America/Chicago
 date_default_timezone_set('America/Chicago');
 
 try{
 
-ob_start();
 if(!isset($_SESSION)) 
 { 
     session_start(); 
 }
 	
-	//require router file
+	//require router file to start routing application
 	require '../app/config/router.php';
-	
-	
-	new Router(@$_GET['url']);
-	
 
-	
+	//starting router class with get method from URL
+	new Router(@$_GET['url']);
 	
 }
 
+//if there is any error/exception sent by application, catch it
+//show the error and kill the page
 catch(Exception $e){
 	echo $e->getMessage();
 	die();
